02a33c0e5a9b79f2e894ea00c14089a4841739d3
BriFBoy <98982693+BriFBoy@users.noreply.github.com>
2026-04-21 12:39:03 +0200

    added distro and hostname

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -13,18 +13,24 @@ pub struct AppData {
 pub struct SystemInfo {
     mem_total: u64,
     disk_total: u64,
+    distro: String,
+    hostname: String,
 }
 
 impl SystemInfo {
-    pub fn new(mem_total: u64, disk_total: u64) -> SystemInfo {
+    pub fn new(mem_total: u64, disk_total: u64, distro: String, hostname: String) -> SystemInfo {
         SystemInfo {
             mem_total,
             disk_total,
+            distro,
+            hostname,
         }
     }
     pub fn from_agent_response(raw: &str) -> Self {
         let mut mem_total = 0u64;
         let mut disk_total = 0u64;
+        let mut distro = String::new();
+        let mut hostname = String::new();
         raw.split(";").for_each(|pair| {
             let parts: Vec<&str> = pair.split('=').collect();
             if parts.len() != 2 {
@@ -34,10 +40,12 @@ impl SystemInfo {
             match key {
                 "mem" => mem_total = value.parse().unwrap_or(0),
                 "disk" => disk_total = value.parse().unwrap_or(0),
+                "distro" => distro = value.to_owned(),
+                "hostname" => hostname = value.to_owned(),
                 _ => {}
             }
         });
-        SystemInfo::new(mem_total, disk_total)
+        SystemInfo::new(mem_total, disk_total, distro, hostname)
     }
 }
 
